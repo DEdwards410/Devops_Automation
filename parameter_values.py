02c25bb953060fcdd74deb5b7d83decde0bcadbe
DEdwards410 <85324842+DEdwards410@users.noreply.github.com>
2022-06-23 19:44:26 -0400

    Add files via upload

---
+++ parameter_values.py
@@ -0,0 +1,38 @@
+'''
+This script takes the input from a csv file (name of parameter and parameter vale)
+and updates the value in parameter store, overwriting the current value and reflecting
+that to an updated version number. Change line 17 to the approiate name of the input file.
+(will make script more dynamic for file input at later time)  
+'''
+
+
+import boto3
+import csv
+
+
+client = boto3.client('ssm')
+
+List_of_Names = []
+List_of_Value = []
+with open('secrets.csv') as file:
+    secrets = csv.reader(file)
+    for row in secrets:
+        Name = row[0]
+        Value = row[1]
+        List_of_Names.append(Name)
+        List_of_Value.append(Value)
+
+
+for i in range(len(List_of_Names)):
+    print(List_of_Names[i] + '       \t       ' + List_of_Value[i])
+
+
+for x in range(1, len(List_of_Names)):
+    response = client.put_parameter(Name=List_of_Names[x], Value=List_of_Value[x],
+                                    Type='String', Overwrite=True,
+                                    # AllowedPattern='string',
+                                    Tier='Standard', DataType='text'
+                                    )
+
+for r in response:
+    print(response)
